d51212475d0a72f106dec9667ff59c830422fa00
Nicolas Collins <collins.nicolas@gmail.com>
2015-05-19 22:19:40 -0400

    added process run

--- src/TimestampCapture.java
+++ src/TimestampCapture.java
@@ -1,5 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by ncollins on 5/19/2015.
  */
 public class TimestampCapture {
+    public String getTimestamps(){
+        String s = null;
+        String command;
+
+        try {
+            // using the Runtime exec method:
+            Process p = Runtime.getRuntime().exec(command);
+
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(p.getErrorStream()));
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+        }
+        catch (IOException e) {
+            System.out.println("exception happened - here's what I know: ");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
 }
